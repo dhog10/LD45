1fa7f7c1f3e85b4bffac950809110997c38a425d
William <u1563819@unimail.hud.ac.uk>
2019-10-05 13:56:15 +0100

    Added Click Delegation

---
+++ Assets/scripts/ClickDelegate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ClickDelegate : MonoBehaviour
+{
+    [SerializeField] private int pickUpRange;
+
+    public delegate void Clicker(GameObject obj, RaycastHit rayHit);
+    public event Clicker OnClick;
+
+    private static ClickDelegate instance;
+    
+    private ClickDelegate() { }
+
+    public static ClickDelegate Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+
+                instance = GameObject.FindObjectOfType(typeof(ClickDelegate)) as ClickDelegate;
+
+            }
+
+            return instance;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(raycast, out rayHit, pickUpRange))
+        {
+            // If we click it
+            if (Input.GetButtonDown("Fire1"))
+            {
+                // Notify of the event!
+                OnClick(rayHit.transform.gameObject, rayHit);
+
+            }
+        }
+    }
+}
