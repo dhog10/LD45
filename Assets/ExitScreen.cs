c339023a441b7f44d4f063afaa6788993ab1ecf3
dhog10 <dhog10@googlemail.com>
2019-10-07 20:36:51 +0100

    credits

---
+++ Assets/ExitScreen.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScreen : MonoBehaviour
+{
+    public AudioSource cinematicHit;
+    public AudioSource creditMusic;
+    public GameObject canvasObject;
+    public GameObject creditsObject;
+
+    private bool creditsActive;
+    private float creditsStarted = 0f;
+    private bool creditMusicPlayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasObject.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(creditsActive)
+        {
+            if(Time.time - creditsStarted > 0.2f && !canvasObject.gameObject.activeSelf)
+            {
+                canvasObject.gameObject.SetActive(true);
+            }
+
+            if(Time.time - creditsStarted > 5f)
+            {
+                if (!creditMusicPlayed)
+                {
+                    creditMusicPlayed = true;
+                    creditMusic.Play();
+                }
+            }
+
+            if(Time.time - creditsStarted > 7f)
+            {
+                if (!creditsObject.activeSelf)
+                {
+                    creditsObject.SetActive(true);
+                }
+            }
+            
+        }
+    }
+
+    public void RollCredits()
+    {
+        cinematicHit.Play();
+
+        creditsActive = true;
+        creditsStarted = Time.time;
+    }
+}
---
+++ Assets/scripts/Credits.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public Image logo;
+    public AudioSource music;
+
+    private float timeExisted;
+
+    private void Update()
+    {
+        if (music.isPlaying)
+        {
+            timeExisted = Mathf.Min(1f, timeExisted + Time.deltaTime * 0.1f);
+        }
+        else
+        {
+            timeExisted = Mathf.Max(0f, timeExisted - Time.deltaTime * 0.3f);
+            if(timeExisted <= 0f)
+            {
+                Application.Quit();
+            }
+        }
+        
+
+        var color = new Color(1f, 1f, 1f, timeExisted);
+
+        logo.color = color;
+
+
+    }
+}
